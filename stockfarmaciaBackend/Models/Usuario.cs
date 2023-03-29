10b42fc5ef1d250d974c57bcd924bf70176ea1ca
elisa-dep1 <e.rojasreyes@uandresbello.edu>
2023-03-29 19:25:58 -0300

    initial

---
+++ stockfarmaciaBackend/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace stockfarmaciaBackend.Models
+{
+    public class Usuario
+    {
+        public String? Nombre { get; set; }
+        public String? Apellido { get; set; }
+
+        public Usuario(string? nombre, string? apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+    }
+}
